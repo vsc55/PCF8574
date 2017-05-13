f03050053357117828107712d1c29456d36ca2bf
vsc55 <jpastor@cerebelum.net>
2017-05-13 17:31:09 +0200

    Update Example.

--- examples/PCF8574FileExample/PCF8574FileExample.ino
+++ examples/PCF8574FileExample/PCF8574FileExample.ino
@@ -3,34 +3,67 @@
 #define I2C_DIRECCION_PCF8574_1 0x38
 #define I2C_DIRECCION_PCF8574_2 0x38
 
-//definimos el objeto
-PCF8574 I2C_PCF8574(I2C_DIRECCION_PCF8574);
+
+/*
+   Crear objeto, por defecto se configurar el bus I2C en modo master.
+   Si le pasamos la dirección se usará esa en el bus I2C.
+
+   I2C_PCF8574(I2C_DIRECCION_PCF8574_1, Optional I2C_ADDRES_DEVICE);
+   
+   Ejemplo:
+      PCF8574 I2C_PCF8574(I2C_DIRECCION_PCF8574_1);
+      PCF8574 I2C_PCF8574(I2C_DIRECCION_PCF8574_1, 0x11);
+*/
+PCF8574 I2C_PCF8574(I2C_DIRECCION_PCF8574_1);           //MASTER
+
 
 void setup() {
   Serial.begin(9600);
 
-  //PONE TODOS LOS PINES EN FALSE
-  IC_PCF8574.ResetPinStatus();
 
-  //PONE TODOS LOS PINES A TURE
-  IC_PCF8574.SetPinStatus(0, true);
+  /*
+     Reseteamos todos los canales y se ponen en modo False.
+  */
+  I2C_PCF8574.ResetPinStatus();
 
-  //PONE EL PIN 1 Y 6 EN FALSE
-  I2C_PCF8574.SetPinStatus(1, false);
-  I2C_PCF8574.SetPinStatus(6, false);
 
-  //leemos el estado del pin 5 y 6 que tiene un valor de true y false respectivamente.
+  /*
+     Ponemos todos los canales en modo True.
+     Nota: El canal numero 0 está reservado y se usa para referirse a todos los canales.
+  */
+  I2C_PCF8574.SetPinStatus(0, CANAL_STATUS_ON);
+
+
+  /*
+     Ponemos los canales 1 y 6 en modo False.
+  */
+  I2C_PCF8574.SetPinStatus(1, CANAL_STATUS_OFF);
+  I2C_PCF8574.SetPinStatus(6, CANAL_STATUS_OFF);
+
+
+  /*
+     Leemos el estado de los canales 5 y 7 que ahora tiene un valor de true y false respetivamente.
+  */
   int pin5 = I2C_PCF8574.ReadPinStatus(5);
   int pin6 = I2C_PCF8574.ReadPinStatus(6);
 
-  //cambiamos del la direccion I2C del PCF8574 para poder ablar con otro PCF8574.
-  I2C_PCF8574.SetAddress(I2C_DIRECCION_PCF8574_2)
 
-  //ahora reseteamos todos los pines del PCF8574 del segundo canal I2C.
-  IC_PCF8574.ResetPinStatus();
+  /*
+     Cambiamos la dirección I2C para poder controlar otro PCF8574.
+  */
+  I2C_PCF8574.SetAddress(I2C_DIRECCION_PCF8574_2);
 
-  //obtenemos la direccion I2C que estamos usando ahora.
-  byte I2C_ADDRESS = I2C_PCF8574.SetAddress();
+
+  /*
+     Reseteamos todos los canales del PCF8574 que tenemos configurado ahora mismo.
+  */
+  I2C_PCF8574.ResetPinStatus();
+
+
+  /*
+     Obtenemos la dirección I2C que está configurada actualmente.
+  */
+  byte I2C_ADDRESS = I2C_PCF8574.GetAddress();
 
 }
 
